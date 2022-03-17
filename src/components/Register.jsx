0af376ab6506ef6a5d142709232d43cbb445087c
Denis Osipov <ddennios@mail.ru>
2022-03-17 18:28:34 +0300

    исправил отступы в app.jsx

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,37 +1,37 @@
 import React from "react";
 
-function Register ({handleRegister, unsuccessfulRegister}) {
+function Register({ handleRegister, unsuccessfulRegister }) {
   const [registerData, setRegisterData] = React.useState({
     email: '',
     password: '',
   });
 
-  const {email, password} = registerData;
+  const { email, password } = registerData;
 
-  function handleChange (e) {
+  function handleChange(e) {
     setRegisterData({
       ...registerData,
-      [e.target.name] : e.target.value
+      [e.target.name]: e.target.value
     })
   }
 
   function handleSubmit(e) {
     e.preventDefault();
     handleRegister(email, password)
-    .catch((e) => unsuccessfulRegister())
+      .catch((e) => unsuccessfulRegister())
   }
 
-    return (
-        <div className="register">
-        <h2 className="register__title">Регистрация</h2>
-          <form onSubmit={handleSubmit} name="form_register" className="form register__content">
-              <input className="register__input" name="email" value={email} onChange={handleChange} type="text" placeholder="Email"></input>
-              <input className="register__input" name="password" value={password} onChange={handleChange} type="password" placeholder="пароль"></input>
-            <button type="submit" className="register__button register__button_submit">Зарегистрироваться</button>
-          </form>
-          <h3 className="register__caption">Уже зарегистрированы ?<button className="register__button_enter" type="button">Войти</button></h3>
-        </div>
-    );
+  return (
+    <div className="register">
+      <h2 className="register__title">Регистрация</h2>
+      <form onSubmit={handleSubmit} name="form_register" className="form register__content">
+        <input className="register__input" name="email" value={email} onChange={handleChange} type="text" placeholder="Email"></input>
+        <input className="register__input" name="password" value={password} onChange={handleChange} type="password" placeholder="пароль"></input>
+        <button type="submit" className="register__button register__button_submit">Зарегистрироваться</button>
+      </form>
+      <h3 className="register__caption">Уже зарегистрированы ?<button className="register__button_enter" type="button">Войти</button></h3>
+    </div>
+  );
 }
 
 export default Register;
